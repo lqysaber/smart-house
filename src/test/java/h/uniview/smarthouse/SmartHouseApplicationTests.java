e440bd5a6acf053e1e442c859486857d206ba0df
lqysaber <kbtjhvb@yeah.net>
2019-07-11 01:16:28 +0800

    initial uniview smart house project

---
+++ src/test/java/h/uniview/smarthouse/SmartHouseApplicationTests.java
@@ -0,0 +1,16 @@
+package h.uniview.smarthouse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartHouseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
