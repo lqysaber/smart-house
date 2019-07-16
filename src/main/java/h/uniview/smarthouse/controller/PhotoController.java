072c7e740348317905a3f9200631979a5b46cabd
lqysaber <kbtjhvb@yeah.net>
2019-07-17 02:38:58 +0800

    initial uniview smart house project

--- src/main/java/h/uniview/smarthouse/controller/PhotoController.java
+++ src/main/java/h/uniview/smarthouse/controller/PhotoController.java
@@ -1,11 +1,9 @@
 package h.uniview.smarthouse.controller;
 
-import javax.validation.Valid;
-
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.LockedAccountException;
-import org.apache.shiro.authc.UnknownAccountException;
+import h.uniview.smarthouse.form.PhotoForm;
+import h.uniview.smarthouse.utils.FileUtils;
+import h.uniview.smarthouse.utils.PageUtils;
+import h.uniview.smarthouse.utils.R;
 import org.springframework.http.MediaType;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,25 +11,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import h.uniview.smarthouse.form.PhotoForm;
-import h.uniview.smarthouse.utils.R;
+import javax.validation.Valid;
+import java.util.List;
 
-@RestController(value = "photo")
+@RestController
 public class PhotoController extends BaseController {
 	
-	@RequestMapping(value = "queryInventory", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(value = "/photo/list", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public R list(@RequestBody @Valid PhotoForm photoForm, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
             return R.error(handleError(bindingResult));
         }
+		String path = "C:\\Users\\HugoLi\\Downloads\\子骏\\入册\\";
         try {
-            return R.ok();
-        } catch (UnknownAccountException | IncorrectCredentialsException | LockedAccountException e) {
+            List<String> result = FileUtils.listFile(path);
+            PageUtils pg = new PageUtils(result, result.size(), photoForm.getPageSize(), photoForm.getCurrPage());
+            return R.ok(pg);
+        } catch (Exception e) {
             return R.error(e.getMessage());
-        } catch (AuthenticationException e) {
-            return R.error("认证失败！");
         }
     }
+
+//    @RequestMapping(value = "/photo/list", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//    @ResponseBody
+//    public R list(int pageSize, int currPage, String queryDate) {
+//        String path = "C:\\Users\\HugoLi\\Downloads\\子骏\\入册\\";
+//        try {
+//            List<String> result = FileUtils.listFile(path);
+//            PageUtils pg = new PageUtils(result, result.size(), pageSize, currPage);
+//            return R.ok(pg);
+//        } catch (Exception e) {
+//            return R.error(e.getMessage());
+//        }
+//    }
 
 }
