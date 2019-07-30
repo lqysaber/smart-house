b1cc366043469c0c846336f558475cfb764fea95
CN092227 <CN092227@cnwsgznsdd620.na.intranet.msd>
2019-07-30 19:24:18 +0800

    add show device detail method

--- src/main/java/h/uniview/smarthouse/controller/PageController.java
+++ src/main/java/h/uniview/smarthouse/controller/PageController.java
@@ -1,14 +1,18 @@
 package h.uniview.smarthouse.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,13 +20,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import h.uniview.smarthouse.data.CameraInfo;
 import h.uniview.smarthouse.data.ConfigMsg;
 import h.uniview.smarthouse.data.EnvCfgCenter;
+import h.uniview.smarthouse.data.NVRInfo;
+import h.uniview.smarthouse.data.ServerNodeInfo;
 import h.uniview.smarthouse.data.VideoNodeInfo;
 import h.uniview.smarthouse.data.WorkstationMsg;
+import h.uniview.smarthouse.exception.RRException;
 import h.uniview.smarthouse.form.CameraForm;
 import h.uniview.smarthouse.form.ConfigDataForm;
+import h.uniview.smarthouse.form.DeviceForm;
+import h.uniview.smarthouse.form.ServerForm;
 import h.uniview.smarthouse.form.VideoForm;
 import h.uniview.smarthouse.form.WorkStationForm;
 import h.uniview.smarthouse.utils.Constant.ConfigEnvType;
+import h.uniview.smarthouse.utils.Constant.DeviceType;
+import h.uniview.smarthouse.utils.Constant;
 import h.uniview.smarthouse.utils.R;
 
 @Controller
@@ -98,6 +109,55 @@ public class PageController extends BaseController {
         } catch (Exception e) {
             return R.error(e.getMessage());
         }
+    }
+    
+    @RequestMapping("/device/show/detail")
+    @ResponseBody
+	public R showDevice(@RequestBody @Valid DeviceForm form, BindingResult bindingResult){
+    	if (bindingResult.hasErrors()) {
+            return R.error(handleError(bindingResult));
+        }
+    	
+    	try {
+	    	if(DeviceType.CAMERA.getValue().equals(form.getType())) {
+	    		return R.ok(retrieveDevice(form.getCursor(), CameraInfo.class));
+			} 
+	    	if(DeviceType.NVR.getValue().equals(form.getType())) {
+	    		return R.ok(retrieveDevice(form.getCursor(), NVRInfo.class));
+			} 
+	    	if(DeviceType.VIDEO_BAL.getValue().equals(form.getType()) || DeviceType.VIDEO_GUN.getValue().equals(form.getType())) {
+	    		return R.ok(retrieveDevice(form.getCursor(), VideoNodeInfo.class));
+			} 
+	    	return R.ok(retrieveDevice(form.getCursor(), ServerNodeInfo.class));
+    	} catch (Exception e) {
+    		return R.error(e.getMessage());
+    	}
+	}
+    
+    private <T> T retrieveDevice(String cursor, Class<T> clazz) throws Exception {
+    	int idx = Integer.parseInt(cursor);
+    	if(clazz.equals(CameraInfo.class)) {
+    		if(envCenter.getCameraInfoList().size() < idx+1) {
+    			throw new RRException("System Error");
+    		}
+    		return (T) envCenter.getCameraInfoList().get(idx);
+    	} 
+    	if(clazz.equals(NVRInfo.class)) {
+    		if(envCenter.getNvrInfoList().size() < idx+1) {
+    			throw new RRException("System Error");
+    		}
+    		return (T) envCenter.getNvrInfoList().get(idx);
+    	} 
+    	if(clazz.equals(VideoNodeInfo.class)) {
+    		if(envCenter.getVideoInfoList().size() < idx+1) {
+    			throw new RRException("System Error");
+    		}
+    		return (T) envCenter.getVideoInfoList().get(idx);
+    	} 
+		if(envCenter.getServerNodeList().size() < idx+1) {
+			throw new RRException("System Error");
+		}
+		return (T) envCenter.getServerNodeList().get(idx);
     }
 	
 }
